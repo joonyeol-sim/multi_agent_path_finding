db3dc567bd63ad19b46a966ae559783610e43e89
SimJoonYeol <antihero1290@gmail.com>
2023-09-12 14:19:04 +0900

    Update to reduced name

---
+++ ecbs/conflict_based_search.py
@@ -0,0 +1,137 @@
+from queue import PriorityQueue
+from typing import List, Tuple
+from itertools import combinations
+
+
+from common.environment import Environment
+from common.point import Point
+from common.conflict import Conflict, VertexConflict, EdgeConflict
+from common.constraint import (
+    Constraint,
+    VertexConstraint,
+    EdgeConstraint,
+)
+from conflict_based_search.constraint_tree_node import CTNode
+from space_time_astar.space_time_astar import SpaceTimeAstar
+
+
+class ConflictBasedSearch:
+    def __init__(
+        self, start_points: List[Point], goal_points: List[Point], env: Environment
+    ):
+        # check if the length of start_points and goal_points are the same
+        if len(start_points) != len(goal_points):
+            raise ValueError(
+                f"Length of start_points and goal_points are not the same: {len(start_points)} != {len(goal_points)}"
+            )
+
+        self.start_points = start_points
+        self.goal_points = goal_points
+        self.robot_num = len(start_points)
+        self.env = env
+
+        self.constraints_tree = PriorityQueue()
+        self.individual_planners = [
+            SpaceTimeAstar(start_point, goal_point, env)
+            for start_point, goal_point in zip(start_points, goal_points)
+        ]
+
+    def plan(self):
+        constraints: List[Constraint] = []
+        solution: List[List[Tuple[Point, int]]] = [
+            self.individual_planners[i].plan()
+            for i in range(len(self.individual_planners))
+        ]
+        cost: int = self.calculate_cost(solution)
+        root_node = CTNode(constraints, solution, cost)
+        self.constraints_tree.put(root_node)
+        while not self.constraints_tree.empty():
+            cur_node = self.constraints_tree.get()
+            conflict = self.find_first_conflict(cur_node.solution)
+            if not conflict:
+                return cur_node.solution
+            for agent_id in conflict.agent_ids:
+                constraint = self.generate_constraint_from_conflict(agent_id, conflict)
+
+                new_constraints = cur_node.constraints.copy()
+                new_constraints.append(constraint)
+                new_solution = cur_node.solution.copy()
+                new_solution[agent_id] = self.individual_planners[agent_id].plan(
+                    constraints=new_constraints
+                )
+                if not new_solution[agent_id]:
+                    continue
+                new_cost = self.calculate_cost(new_solution)
+                new_node = CTNode(new_constraints, new_solution, new_cost)
+                self.constraints_tree.put(new_node)
+        return None
+
+    @staticmethod
+    def generate_constraint_from_conflict(
+        agent_id: int, conflict: Conflict
+    ) -> Constraint:
+        if isinstance(conflict, VertexConflict):
+            return VertexConstraint(
+                agent_id=agent_id,
+                point=conflict.point,
+                time=conflict.time,
+            )
+        elif isinstance(conflict, EdgeConflict):
+            return EdgeConstraint(
+                agent_id=agent_id,
+                points=conflict.points[agent_id],
+                times=conflict.times,
+            )
+        else:
+            raise ValueError(f"Unknown conflict type: {type(conflict)}")
+
+    @staticmethod
+    def get_state(agent_id: int, time: int, solution: List[List[Tuple[Point, int]]]):
+        if time >= len(solution[agent_id]):
+            return solution[agent_id][-1]
+        else:
+            return solution[agent_id][time]
+
+    def calculate_cost(self, solution: List[List[Tuple[Point, int]]]) -> int:
+        cost = 0
+        for i in range(self.robot_num):
+            cost += len(solution[i])
+        return cost
+
+    def find_first_conflict(self, solution: List[List[Tuple[Point, int]]]) -> Conflict:
+        # Vertex Conflict
+        for agent1, agent2 in combinations(range(self.robot_num), 2):
+            max_time = max(len(solution[agent1]), len(solution[agent2]))
+            for time in range(max_time):
+                point1, time1 = self.get_state(agent1, time, solution)
+                point2, time2 = self.get_state(agent2, time, solution)
+                if point1 == point2 and time1 == time2:
+                    return VertexConflict(
+                        agent_ids=[agent1, agent2],
+                        point=point1,
+                        time=time1,
+                    )
+
+        # Edge Conflict
+        for agent1, agent2 in combinations(range(self.robot_num), 2):
+            max_time = max(len(solution[agent1]), len(solution[agent2]))
+            for time in range(max_time - 1):
+                prev_point1, prev_time1 = self.get_state(agent1, time, solution)
+                next_point1, next_time1 = self.get_state(agent1, time + 1, solution)
+                prev_point2, prev_time2 = self.get_state(agent2, time, solution)
+                next_point2, next_time2 = self.get_state(agent2, time + 1, solution)
+
+                if (
+                    prev_point1 == next_point2
+                    and prev_point2 == next_point1
+                    and prev_time1 == prev_time2
+                    and next_time1 == next_time2
+                ):
+                    return EdgeConflict(
+                        agent_ids=[agent1, agent2],
+                        points={
+                            agent1: (prev_point1, next_point1),
+                            agent2: (prev_point2, next_point2),
+                        },
+                        times=(prev_time1, next_time1),
+                    )
---
+++ stastar_epsilon/stastar_epsilon.py
@@ -0,0 +1,123 @@
+from typing import List, Set, Tuple
+
+from common.environment import Environment
+from common.point import Point
+from common.constraint import Constraint, VertexConstraint, EdgeConstraint
+from space_time_astar.node import Node
+
+
+class SpaceTimeAstar:
+    def __init__(self, start_point: Point, goal_point: Point, env: Environment):
+        self.env = env
+        self.start_point = start_point
+        self.goal_point = goal_point
+        if env.dimension != len(start_point.__dict__.keys()):
+            raise ValueError(
+                f"Dimension does not match the length of start: {start_point}"
+            )
+        if env.dimension != len(goal_point.__dict__.keys()):
+            raise ValueError(
+                f"Dimension does not match the length of goal: {goal_point}"
+            )
+        if not self.is_valid_point(start_point, 0):
+            raise ValueError(f"Start point is not valid: {start_point}")
+        if not self.is_valid_point(goal_point, 0):
+            raise ValueError(f"Goal point is not valid: {goal_point}")
+
+    def plan(
+        self, constraints: List[Constraint] = None
+    ) -> List[Tuple[Point, int]] | None:
+        open_set: Set[Node] = set()
+        open_set.add(Node(self.start_point, 0))
+        while open_set:
+            current = min(open_set)
+            open_set.remove(current)
+            if current.point == self.goal_point:
+                return self.reconstruct_path(current)
+            neighbors = self.get_neighbors(current, constraints)
+            for neighbor in neighbors:
+                if neighbor not in open_set:
+                    open_set.add(neighbor)
+                    neighbor.parent = current
+                    neighbor.g_score = current.g_score + 1
+                    neighbor.h_score = self.heuristic(neighbor)
+                    neighbor.f_score = neighbor.g_score + neighbor.h_score
+
+                if current.g_score + 1 < neighbor.g_score:
+                    neighbor.parent = current
+                    neighbor.g_score = current.g_score + 1
+                    neighbor.h_score = self.heuristic(neighbor)
+                    neighbor.f_score = neighbor.g_score + neighbor.h_score
+
+        return None
+
+    def heuristic(self, node) -> int:
+        # return manhattan distance
+        return node.point.manhattan_distance(self.goal_point)
+
+    @staticmethod
+    def reconstruct_path(node: Node) -> List[Tuple[Point, int]]:
+        path: List[Tuple[Point, int]] = [(node.point, node.time)]
+        while node.parent is not None:
+            path.append((node.parent.point, node.parent.time))
+            node = node.parent
+        return path[::-1]
+
+    def get_neighbors(
+        self, node: Node, constraints: List[Constraint] = None
+    ) -> List[Node]:
+        neighbors: List[Node] = []
+        # move action
+        for neighbor_point in node.point.get_neighbor_points():
+            if self.is_valid_point(
+                neighbor_point, node.time + 1
+            ) and self.is_valid_given_constraints(
+                node.point, neighbor_point, node.time, node.time + 1, constraints
+            ):
+                neighbors.append(Node(neighbor_point, node.time + 1))
+
+        # wait action
+        if self.is_valid_point(
+            node.point, node.time + 1
+        ) and self.is_valid_given_constraints(
+            node.point, node.point, node.time, node.time + 1, constraints
+        ):
+            neighbors.append(Node(node.point, node.time + 1))
+        return neighbors
+
+    def is_valid_point(self, point: Point, time: int) -> bool:
+        if not self.is_valid_space(point):
+            return False
+        for obstacle in self.env.obstacles:
+            if obstacle.is_colliding(point=point, time=time):
+                return False
+        return True
+
+    @staticmethod
+    def is_valid_given_constraints(
+        prev_point: Point,
+        next_point: Point,
+        prev_time: int,
+        next_time: int,
+        constraints: List[Constraint],
+    ) -> bool:
+        if constraints is not None:
+            for constraint in constraints:
+                if isinstance(constraint, VertexConstraint):
+                    if constraint.point == next_point and constraint.time == next_time:
+                        return False
+                elif isinstance(constraint, EdgeConstraint):
+                    if (
+                        constraint.points[0] == prev_point
+                        and constraint.points[1] == next_point
+                        and constraint.times[0] == prev_time
+                        and constraint.times[1] == next_time
+                    ):
+                        return False
+        return True
+
+    def is_valid_space(self, point: Point) -> bool:
+        for i, coordinate in enumerate(point.__dict__.values()):
+            if coordinate < 0 or coordinate >= self.env.space_limit[i]:
+                return False
+        return True
